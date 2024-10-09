a12bd248f573e4ea3771473ef443c4a20ba1ba58
Saravia01 <147068606+Saravia01@users.noreply.github.com>
2024-10-08 19:23:58 -0600

    v40

--- Startup.cs
+++ Startup.cs
@@ -64,15 +64,13 @@ namespace marcatel_api
             services.AddSingleton<ModuloService>();
             services.AddSingleton<SucursalesService>();
             services.AddSingleton<CatModuloService>();
-
+            services.AddSingleton<InsumosService>();
             services.AddSingleton<ProveedoresService>();
-
+            services.AddSingleton<DetalleOrdenCompraService>();
             services.AddSingleton<EntradasService>();
             services.AddSingleton<DetalleEntradaService>();
             services.AddSingleton<BancosService>();
             services.AddSingleton<RecetasService>();
-
-
             services.AddSingleton<LoginService>();
 
 
