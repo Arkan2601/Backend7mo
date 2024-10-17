9cc25a1007db2657a9bdb9c2fd738273bdcdb838
Saravia01 <147068606+Saravia01@users.noreply.github.com>
2024-10-17 03:50:38 -0600

    v55

---
+++ Models/DetalleTraspasoModel.cs
@@ -0,0 +1,43 @@
+using System;
+namespace marcatel_api.Models
+{
+    public class GetDetalleTraspasoModel
+    {
+        public int Id { get; set; }
+        public int IdTraspaso { get; set; }
+        public string Insumo {get; set;}
+        public decimal CantidadEnviada {get; set;}
+        public decimal CatidadRecibida {get; set;}
+        public string FechaRegistro {get; set;}
+        public string FechaActualiza {get; set;}
+        public string UsuarioActualiza {get; set;}
+
+
+    }
+
+    public class InsertDetalleTraspasoModel
+    {
+        public int IdTraspaso {get; set;}
+        public string Insumo {get; set;}
+        public decimal CantidadEnviada {get; set;}
+        public int UsuarioActualiza {get; set;}
+        
+    }
+
+    public class UpdateDetalleTraspasoModel
+    {
+        public int Id { get; set; }
+        public string Insumo { get; set; }
+        public decimal CantidadEnviada { get; set; }
+        public decimal CantidadRecibida{ get; set;}
+        public int UsuarioActualiza { get; set; }
+
+    }
+
+    public class DeleteDetalleTraspasoModel
+    {
+        public int Id { get; set; }
+    }
+
+
+}
